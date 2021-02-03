992df611261783828a82947835b2a49498d03fcd
yamanidev <elamine.yamani@gmail.com>
2021-02-03 21:42:34 +0100

    Color change every second, task 1 completed

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
 // Wait a certain time
 // Change its color back to the original one
 // Repeat steps
+boxList = document.getElementsByClassName("box");
+
+function toBlue(){
+    let pos = Math.floor(Math.random() * 10);
+    function toOrigin(){
+        boxList[pos].style.backgroundColor = "antiquewhite";
+    }
+    boxList[pos].style.backgroundColor = 'blue';
+    setTimeout(toOrigin, 1000);
+}
+
+setInterval(toBlue, 1000);
