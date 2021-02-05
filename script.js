013564f300363d8af818b19cf33bd2c9a329aa9f
yamanidev <elamine.yamani@gmail.com>
2021-02-05 22:48:32 +0100

    Click functionality

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Add event listener to selected box
 // Counter for number of clicked boxes on time
 boxList = document.getElementsByClassName("box");
 
 function toBlue(){
     let pos = Math.floor(Math.random() * 10);
+    boxList[pos].addEventListener('click', toOrigin);
     function toOrigin(){
         boxList[pos].style.backgroundColor = "antiquewhite";
     }
